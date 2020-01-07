274e9ca7a9e260dd1775d183c5d76f8641294e76
Alex Abdugafarov <fswork90@gmail.com>
2020-01-07 07:02:02 +0500

    Logging DB load time

--- src/main/scala/org/fs/chm/loader/DataLoader.scala
+++ src/main/scala/org/fs/chm/loader/DataLoader.scala
@@ -4,11 +4,15 @@ import java.io.File
 import java.io.FileNotFoundException
 
 import org.fs.chm.dao.ChatHistoryDao
+import org.fs.utility.StopWatch
+import org.slf4s.Logging
 
-trait DataLoader[D <: ChatHistoryDao] {
+trait DataLoader[D <: ChatHistoryDao] extends Logging {
   def loadData(path: File): D = {
-    if (!path.exists()) throw new FileNotFoundException(s"File ${path.getAbsolutePath} not found")
-    loadDataInner(path)
+    StopWatch.measureAndCall {
+      if (!path.exists()) throw new FileNotFoundException(s"File ${path.getAbsolutePath} not found")
+      loadDataInner(path)
+    }((_, t) => log.info(s"File ${path.getAbsolutePath} loaded in $t ms"))
   }
 
   protected def loadDataInner(path: File): D
