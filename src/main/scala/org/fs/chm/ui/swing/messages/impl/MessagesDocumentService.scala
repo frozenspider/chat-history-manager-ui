frozenspider/chat-history-manager-ui

commit b5ee5cc2065a243d514cb65a3a568fecd2630ce1

Fixed < character getting lost during rendering. Emojis are still not visible :(

src/main/scala/org/fs/chm/ui/swing/messages/impl/MessagesDocumentService.scala
@@ -24,6 +24,13 @@ class MessagesDocumentService(htmlKit: HTMLEditorKit) {
     // TODO: How to limit width and do word wraps?
     //       width, max-width acts weird,
     //       word-wrap, word-break, overflow-wrap are ignored
+    // TODO: Handle emojis!
+    //       Importing font with
+    //         @font-face {
+    //           font-family: my-font;
+    //           src: url(/full/path/to/font);
+    //         }
+    //       doesn't seem to work
     val doc     = htmlKit.createDefaultDocument().asInstanceOf[HTMLDocument]
     val content = """<div id="messages"></div>"""
     htmlKit.read(new StringReader(content), doc, 0)
@@ -269,16 +276,21 @@ class MessagesDocumentService(htmlKit: HTMLEditorKit) {
       }
     }
 
+    /** Replace non-HTML-renderable characters with, well, renderable */
+    private def toHtmlPlaintext(text: String): String = {
+      text replace ("\n", "<br>") replace ("<", "&lt;") replace (">", "&gt;")
+    }
+
     private def renderComponent(rt: RichTextElement): String = {
       val text = rt.textOrEmptyString
       if (rt.`val`.isEmpty) {
         text
       } else rt.`val`.value match {
-        case _: RtePlain         => text replace ("\n", "<br>")
-        case _: RteBold          => s"<b>${text replace ("\n", "<br>")}</b>"
-        case _: RteItalic        => s"<i>${text replace ("\n", "<br>")}</i>"
-        case _: RteUnderline     => s"<u>${text replace ("\n", "<br>")}</u>"
-        case _: RteStrikethrough => s"<strike>${text replace ("\n", "<br>")}</strike>"
+        case _: RtePlain         => toHtmlPlaintext(text)
+        case _: RteBold          => s"<b>${toHtmlPlaintext(text)}</b>"
+        case _: RteItalic        => s"<i>${toHtmlPlaintext(text)}</i>"
+        case _: RteUnderline     => s"<u>${toHtmlPlaintext(text)}</u>"
+        case _: RteStrikethrough => s"<strike>${toHtmlPlaintext(text)}</strike>"
         case link: RteLink       => renderLink(rt, link)
         case _: RtePrefmtBlock   => s"""<pre>${text}</pre>"""
         case _: RtePrefmtInline  => s"""<code>${text}</code>"""

src/main/scala/org/fs/chm/ui/swing/messages/impl/MessagesAreaContainer.scala
@@ -244,7 +244,8 @@ object MessagesAreaContainer {
       val users = (1 to 2) map (createUser(ds.uuid, _))
       val msgs = IndexedSeq(
         {
-          val text = Seq(RichText.makePlain(s"Join the call!"));
+          val text = Seq(RichText.makePlain(s"Join the call! < Emoji: >👍❤️😄<"));
+          // val text = Seq(RichText.makePlain( "An &#128512;awesome &#128515;string with a few &#128521;emojis!"));
           val typed = Message.Typed.Service(MessageService(MessageService.Val.GroupCall(MessageServiceGroupCall(
             members = users map (_.prettyName)
           ))))

src/main/scala/org/fs/chm/ui/swing/messages/impl/MessagesAreaEnhancedContainer.scala
@@ -12,7 +12,6 @@ import com.github.nscala_time.time.Imports._
 import javax.swing.text.Element
 import javax.swing.text.html.HTML
 import javax.swing.text.html.HTMLEditorKit
-import org.fs.chm.dao.ChatHistoryDao
 import org.fs.chm.ui.swing.general.SwingUtils._
 import org.fs.chm.ui.swing.messages.MessageNavigationCallbacks
 import org.fs.utility.Imports._

commit 38930082d241e821480e78fca2109432b7c9c69d

Fixed incorrect HTML escapes introduced in b5ee5cc2065a243d514cb65a3a568fecd2630ce1

src/main/scala/org/fs/chm/ui/swing/messages/impl/MessagesDocumentService.scala
@@ -278,7 +278,7 @@ class MessagesDocumentService(htmlKit: HTMLEditorKit) {
 
     /** Replace non-HTML-renderable characters with, well, renderable */
     private def toHtmlPlaintext(text: String): String = {
-      text replace ("\n", "<br>") replace ("<", "&lt;") replace (">", "&gt;")
+      text replace ("<", "&lt;") replace (">", "&gt;") replace ("\n", "<br>")
     }
 
     private def renderComponent(rt: RichTextElement): String = {